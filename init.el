f8b5c4d3c1d9b300881bd59c9359f9176ba49cdd
Dmitry <klementievd08@yandex.ru>
2025-09-05 20:11:54 +0500

    Something

--- init.el
+++ init.el
@@ -13,3 +13,18 @@
 (if after-init-time
     (vertico-mode 1)
   (after-init! (vertico-mode 1)))
+
+(autoload 'avy-goto-char-2 "avy")
+
+(bind-key "C-'" 'avy-goto-char-2 prog-mode-map)
+(bind-key "C-'" 'avy-goto-word-0 text-mode-map)
+
+(autoload 'ace-window "ace-window")
+(autoload 'ace-swap-window "ace-window")
+(autoload 'ace-delete-window "ace-window")
+
+(bind-key* "M-o M-c" 'ace-window global-map)
+(bind-key* "M-o M-s" 'ace-swap-window global-map)
+(bind-key* "M-o M-d" 'ace-delete-window global-map)
+(bind-key* "M-o M-v" 'split-window-vertically)
+(bind-key* "M-o M-h" 'split-window-horizontally)
